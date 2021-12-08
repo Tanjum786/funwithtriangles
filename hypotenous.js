e3f0f4aa70143ed3c8fdd340084e331799b6cc4f
Tanjum kadakol <84380051+Tanjum786@users.noreply.github.com>
2021-12-08 14:58:31 +0530

    doc

--- hypotenous.js
+++ hypotenous.js
@@ -1,18 +1,18 @@
-var sideInput=document.querySelectorAll(".side-input");
-var hypotenouseBtn=document.querySelector("#hypotenuse-btn");
-var output2=document.querySelector("#output");
+var sideInput = document.querySelectorAll(".side-input");
+var hypotenouseBtn = document.querySelector("#hypotenuse-btn");
+var output2 = document.querySelector("#output");
 
 
-function calculatesumofsquare(a,b){
-    var sumOfSquare=a*a+b*b;
+function calculatesumofsquare(a, b) {
+    var sumOfSquare = a * a + b * b;
     return sumOfSquare;
 
 }
 
-function calculateHypotenouse(){
-    var sumOfSquare= calculatesumofsquare(Number(sideInput[0].value),Number(sideInput[1].value))
-    lengthOfhypotenouse=Math.sqrt(sumOfSquare);
-    output2.innerText= "the length of Hypotenouse "+lengthOfhypotenouse
+function calculateHypotenouse() {
+    var sumOfSquare = calculatesumofsquare(Number(sideInput[0].value), Number(sideInput[1].value))
+    lengthOfhypotenouse = Math.sqrt(sumOfSquare);
+    output2.innerText = "the length of Hypotenouse " + lengthOfhypotenouse
 };
 
-hypotenouseBtn.addEventListener("click",calculateHypotenouse);
+hypotenouseBtn.addEventListener("click", calculateHypotenouse);
--- quiz.js
+++ quiz.js
@@ -1,6 +1,6 @@
-var quizForm=document.querySelector(".quiz-form");
-var submitAnswerbtn=document.querySelector("#submit");
-var output1=document.querySelector("#output");
+var quizForm = document.querySelector(".quiz-form");
+var submitAnswerbtn = document.querySelector("#submit");
+var output1 = document.querySelector("#output");
 
 const correctAnswers = [
     "90°",
@@ -13,24 +13,24 @@ const correctAnswers = [
     "a + b + c",
     "no",
     "45°",
-  ];
-  
-function calculateScore(){
-    var score=0;
-    var index=0;
-    var formresult=new FormData(quizForm)
-    for(var value of formresult.values()){
+];
+
+function calculateScore() {
+    var score = 0;
+    var index = 0;
+    var formresult = new FormData(quizForm)
+    for (var value of formresult.values()) {
         console.log(value);
-        if (value===correctAnswers[index]) {
-            score=score+1
-        console.log(score);
-            
+        if (value === correctAnswers[index]) {
+            score = score + 1
+            console.log(score);
+
         }
         index++
     }
     // console.log(score);
-    output1.innerText="Your score is "+score
+    output1.innerText = "Your score is " + score
 
 }
 
-submitAnswerbtn.addEventListener("click",calculateScore)
+submitAnswerbtn.addEventListener("click", calculateScore)
